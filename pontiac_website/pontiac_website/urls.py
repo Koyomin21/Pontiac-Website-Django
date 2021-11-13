0f86556dc819bd700feb91a1c030a8d4573a01d8
Anuar <anuarborawork@gmail.com>
2021-11-13 16:23:35 +0600

    Added pages app and static files

--- pontiac_website/pontiac_website/urls.py
+++ pontiac_website/pontiac_website/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls'))
+    path('catalog/', include('catalog.urls')),
+    path('', include("pages.urls"))
 ]
