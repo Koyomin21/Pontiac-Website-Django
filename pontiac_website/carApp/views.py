8b4de96e147bd052a5ce9e4d48a5fde78ab01ec0
Ануар Борангазиев <Anuar.Borangaziev@sulpak.kz>
2021-12-21 15:19:02 +0600

    display dynamic data from db(fetching cars and images)

--- pontiac_website/carApp/views.py
+++ pontiac_website/carApp/views.py
@@ -1,5 +1,8 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from .models import CarImage
+from .models import Car
+
 
 # Create your views here.
 def index(request):
@@ -21,48 +24,53 @@ def history(request):
 
 def car_catalog(request):
     
-    cars = [
-        {
-            'name':'PONTIAC GTO',
-            'price':5050,
-            'image_path':'images/car.jpg'
-        },
-        {
-            'name':'PONTIAC GTO',
-            'price':5050,
-            'image_path':'images/car.jpg'
-        },
-        {
-            'name':'PONTIAC GTO',
-            'price':5050,
-            'image_path':'images/car.jpg'
-        },
-        {
-            'name':'PONTIAC GTO',
-            'price':5050,
-            'image_path':'images/car.jpg'
-        },
-         {
-            'name':'PONTIAC GTO',
-            'price':5050,
-            'image_path':'images/car.jpg'
-        },
-         {
-            'name':'PONTIAC GTO',
-            'price':5050,
-            'image_path':'images/car.jpg'
-        },
-        {
-            'name':'PONTIAC GTO',
-            'price':5050,
-            'image_path':'images/car.jpg'
-        },
-         {
-            'name':'PONTIAC GTO',
-            'price':5050,
-            'image_path':'images/car.jpg'
-        }
-    ]
+    # cars = [
+    #     {
+    #         'name':'PONTIAC GTO',
+    #         'price':5050,
+    #         'image_path':'images/car.jpg'
+    #     },
+    #     {
+    #         'name':'PONTIAC GTO',
+    #         'price':5050,
+    #         'image_path':'images/car.jpg'
+    #     },
+    #     {
+    #         'name':'PONTIAC GTO',
+    #         'price':5050,
+    #         'image_path':'images/car.jpg'
+    #     },
+    #     {
+    #         'name':'PONTIAC GTO',
+    #         'price':5050,
+    #         'image_path':'images/car.jpg'
+    #     },
+    #      {
+    #         'name':'PONTIAC GTO',
+    #         'price':5050,
+    #         'image_path':'images/car.jpg'
+    #     },
+    #      {
+    #         'name':'PONTIAC GTO',
+    #         'price':5050,
+    #         'image_path':'images/car.jpg'
+    #     },
+    #     {
+    #         'name':'PONTIAC GTO',
+    #         'price':5050,
+    #         'image_path':'images/car.jpg'
+    #     },
+    #      {
+    #         'name':'PONTIAC GTO',
+    #         'price':5050,
+    #         'image_path':'images/car.jpg'
+    #     }
+    # ]
+
+    fetched_cars = Car.objects.all()
+    car_images = CarImage.objects.all()
+    
+    cars = [ {'name':i.name, 'price': i.price, 'image': car_images.filter(car__car_id=i.car_id).first() } for i in fetched_cars ]
     rows = range(len(cars)//3 + len(cars)%3*1)
     print(rows)
 
