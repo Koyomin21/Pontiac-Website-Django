ffb07daa3a9f6a5a5869cb6af6c6e2cd8b0229af
Ануар Борангазиев <Anuar.Borangaziev@sulpak.kz>
2021-12-21 14:07:20 +0600

    registered custom filter. Added dynamic display on cars.html

--- pontiac_website/carApp/views.py
+++ pontiac_website/carApp/views.py
@@ -20,6 +20,55 @@ def history(request):
 
 
 def car_catalog(request):
-    context = {}
+    
+    cars = [
+        {
+            'name':'PONTIAC GTO',
+            'price':5050,
+            'image_path':'images/car.jpg'
+        },
+        {
+            'name':'PONTIAC GTO',
+            'price':5050,
+            'image_path':'images/car.jpg'
+        },
+        {
+            'name':'PONTIAC GTO',
+            'price':5050,
+            'image_path':'images/car.jpg'
+        },
+        {
+            'name':'PONTIAC GTO',
+            'price':5050,
+            'image_path':'images/car.jpg'
+        },
+         {
+            'name':'PONTIAC GTO',
+            'price':5050,
+            'image_path':'images/car.jpg'
+        },
+         {
+            'name':'PONTIAC GTO',
+            'price':5050,
+            'image_path':'images/car.jpg'
+        },
+        {
+            'name':'PONTIAC GTO',
+            'price':5050,
+            'image_path':'images/car.jpg'
+        },
+         {
+            'name':'PONTIAC GTO',
+            'price':5050,
+            'image_path':'images/car.jpg'
+        }
+    ]
+    rows = range(len(cars)//3 + len(cars)%3*1)
+    print(rows)
+
+    context = {
+        'cars': cars,
+        'rows': rows
+    }
 
     return render(request, 'cars.html', context)
